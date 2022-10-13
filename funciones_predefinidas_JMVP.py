50347b5856a1df09a710c434c12c7e940c16d57a
José María Viúdez <jmviudezp2003@gmail.com>
2022-10-13 10:13:49 +0200

    Modificacion JAC funciones_predefinidas.py

--- funciones_predefinidas_JMVP.py
+++ funciones_predefinidas_JMVP.py
@@ -1,6 +1,14 @@
+# Modificacion JMVP
 x = 1
 y = 2
 absoluto=abs(x)
 print(absoluto)
 division_modulo=divmod(x,y)
 print(division_modulo)
+
+# Modificacion JAC hecho por JMVP, porque no está agregado como colaborador
+# en el repositorio de Luisa: https://github.com/luisaparra-info/cursoPythonSAD_LPG
+hexadicimal=hex(x)
+print(hexadicimal)
+octal=oct(x)
+print(octal)
