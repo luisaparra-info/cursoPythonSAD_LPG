4b8235a42bdb29e748843a9ba88a65e2c488a6f8
albaVo <alba12vo@gmail.com>
2022-10-13 10:12:02 +0200

    igual y diferente

---
+++ operadores_booleanos.py
@@ -0,0 +1,10 @@
+print('Introduce el primer número')
+n1=(input())
+print('Introduce el segundo número')
+n2=(input())
+
+if (n1 == n2):
+    print('Los números introducidos son iguales')
+
+if (n1 != n2 ):
+    print('Los números introducidos son diferentes')
