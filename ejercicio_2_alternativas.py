db88f7336b6d2cfa9fba0a79ea06d7bab0e308df
dani_github <danifpgm@gmail.com>
2022-10-20 09:56:30 +0200

    commit

--- ejercicio_2_alternativas.py
+++ ejercicio_2_alternativas.py
@@ -1,6 +1,6 @@
-num=int(input("Introduce el número: "))
+num=int(input("Introduce el número para ver si es par: "))
 
 if num/2==1:
-    print("El número es par")
+    print("El número es par.")
 else:
-    print("El número no es par")
+    print("El número no es par.")
