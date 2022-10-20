ed45a0cb119343dc44d7c0a8a6898dfea843e9b7
José María Viúdez <jmviudezp2003@gmail.com>
2022-10-20 09:13:40 +0200

    Modificacion ejercicios_repetitivas_JMVP.py por JMVP del Grupo 3

--- ejercicios_repetitivas_JMVP.py
+++ ejercicios_repetitivas_JMVP.py
@@ -1,5 +1,3 @@
-# Ejercicios de repetitivas
-
 # 5. Escribe un programa que diga si un número introducido por teclado es o no primo. Un número primo es aquel que sólo es divisible entre él mismo y la unidad.
 
 # Solución
